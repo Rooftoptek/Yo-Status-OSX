c896b115033a84644508d9453ad3ab030947a873
Emmanuel Merali <emmanuel@merali.me>
2016-01-20 02:10:24 +0200

    Initial Commit

---
+++ Yo Status/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yo Status
+//
+//  Created by Emmanuel Merali on 20/01/2016.
+//  Copyright © 2016 backendless.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Yo Status/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Yo Status
+//
+//  Created by Emmanuel Merali on 20/01/2016.
+//  Copyright © 2016 backendless.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
